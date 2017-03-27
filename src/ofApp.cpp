9edd04fcfc032a8580c860ddec8c7d3486eb1111
IslandgirlCoder <IslandgirlCoder@users.noreply.github.com>
2017-03-27 19:07:03 -0400

    App

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -8,12 +8,12 @@ void ofApp::setup()
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
 
     std::random_device rd; // will be used to seed the engine eng1 below
-    std::mt19937 eng1(1337); // this engine produces different ouput every time
-    std::mt19937 eng2(rd());// this engine  produces the same output every time
+    std::mt19937 eng1(1337); // this engine  produces the same output every time
+    std::mt19937 eng2(rd());// this engine produces different ouput every time
 
-    float meanx = 1000.0f; // average
-    float meany = 800.0f; // average
-    float sd = 100.0f; // standard deviation
+    float meanx = 100.0f; // average
+    float meany = 50.0f; // average
+    float sd = 15.0f; // standard deviation
 
     int minx = 0; // minimum number to generate
     int maxx = 1200; // maximum number to generate
@@ -29,22 +29,22 @@ jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" f
     std::uniform_int_distribution<> unifrmy(miny, maxy);
 
   //generate 20 normally distributed floats  
-  for(int n=0 ;  n < 20 ;  n++) 
+  for(int n=0 ;  n < 5 ;  n++) 
    {
      X.push_back(normx(eng1));// set of x-coordinates for jewel
      Y.push_back(normy(eng1));//set of y-coordinates for jewel
     //cout<<norm(eng1)<<" "; // different output each time
-    }
+   }
 
   //generate 20 uniformly distributed integers 
-  //for(int n=0 ;  n < 20 ; n++) 
-  //{
+  for(int n=0 ;  n < 10 ; n++) 
+  {
         
-     //X.push_back(unifrmx(eng2));// set of x-coordinates for jewel
-     //Y.push_back(unifrmy(eng2));//set of y-coordinates for jewel
+     X.push_back(unifrmx(eng2));// set of x-coordinates for jewel
+     Y.push_back(unifrmy(eng2));//set of y-coordinates for jewel
     //cout<<unifrm(eng2)<<" ";// same output each time
-  //}
-
+  }
+}
 //--------------------------------------------------------------
 void ofApp::update(){
 
